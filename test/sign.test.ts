412ddb6d205e7a1dc0881aba6b1df0cfd1b8a29f
adamerikoff <adamerikoff@protonmail.com>
2024-02-29 12:20:34 +0600

    Update sign.test.ts

--- test/sign.test.ts
+++ test/sign.test.ts
@@ -2,6 +2,30 @@ import sign from "../src/sign"
 
 describe("sign", () => {
   it("shoudl produce different signatures for different payloads", () => {
-    expect(true).toBe(true);
+    const jwtOne = sign({
+      payload: {name: "Saud"},
+      secret,
+      options: {expiredIn: 8.64e7},
+
+    }).split(".")[2];
+    const jwtTwo = sign({
+      payload: {name: "Saud"},
+      secret: `${secret}-1323423`,
+      options: {expiredIn: 8.64e7},
+
+    }).split(".")[2];
+
+    expect(jwtOne).not.toBe(jwtTwo);
+  });
+
+  it("shoudl produce different signatures for different payloads", () => {
+    const jwtOne = sign({
+      payload: {name: "Saud"},
+      secret,
+      options: {expiredIn: 8.64e7},
+
+    }).split(".")[1];
+
+    expect(typeof JSON.parse(atob(jwtOne)).expect.toBe(Number))
   });
 });
